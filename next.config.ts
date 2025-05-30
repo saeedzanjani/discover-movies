05648a3b4a9426c770474f75c444e86dd0aa67e3
saeed safari <saeid.elitem@gmail.com>
2025-05-30 20:24:52 +0330

    configuration tailwindCSS and global styles

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "image.tmdb.org",
-        pathname: "/t/p/**",
-      },
-    ],
-    deviceSizes: [96, 128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ["image/webp", "image/avif"],
-    minimumCacheTTL: 3600,
-    unoptimized: process.env.NODE_ENV === "development",
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-  experimental: {
-    optimizeCss: true,
-  },
-  httpAgentOptions: {
-    keepAlive: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
